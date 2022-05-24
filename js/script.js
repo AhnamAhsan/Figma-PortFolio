c45c87d22f06775c3c374d9435a4c7bbc6d4eda8
AhnamAhsan <ahnam.ahan@gmail.com>
2022-05-24 22:40:10 +0600

    v1

---
+++ js/script.js
@@ -0,0 +1,72 @@
+$(function () {
+
+    /* testimonial slider */
+
+    $('.slider-wrapper').slick({
+        prevArrow: '<i class="arrow1 fas fa-arrow-left"></i>',
+        nextArrow: '<i class="arrow2 fas fa-arrow-right"></i>',
+        
+        fade: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        speed: 1000 
+    })
+
+
+
+
+
+/* scroll top button js starts */
+
+    $(window).on('scroll', function() {
+        
+        if($(this).scrollTop() > 450 ) {
+            $('#topbtn').fadeIn(500);
+        }else{
+            $('#topbtn').fadeOut(500);
+        }
+
+    })
+
+    
+    $('#topbtn').on('click', function () {
+        $('html,body').animate({
+            scrollTop: 0,
+        },1000)
+    })
+
+    /* scroll top button js ends */
+
+    /* smooth jump js starts*/ 
+
+    $('a').on('click', function(event){
+        event.preventDefault;
+        if(this.hash !== '') {
+            $('html,body').animate({
+                scrollTop:$(this.hash).offset().top,
+            },1000)
+        }
+    })
+
+    /* smooth jump js ends*/
+
+
+    /*fixed menu*/
+
+   $(window).on('scroll', function(){
+       if ($(window).scrollTop() > $('#navsection').height()){
+           $('#navsection').addClass('fixed-menu')
+       }else {
+           $('#navsection').removeClass('fixed-menu')
+       }
+   })
+
+   /* fixed menu */
+
+
+
+})
+
+
+
+
